ed8c83dcb5f4634aee07a2bbd29e7deb698a36d7
notgurev <notgurev@gmail.com>
2022-01-10 05:22:59 +0300

    init

---
+++ src/main/java/lemsolaris/LemsolarisBackendApplication.java
@@ -0,0 +1,11 @@
+package lemsolaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemsolarisBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LemsolarisBackendApplication.class, args);
+    }
+}
