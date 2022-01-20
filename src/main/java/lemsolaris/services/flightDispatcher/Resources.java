c97e926de943f57518c330b935d94f2fc96edac5
HusseinHalifaev <a.alibekovich@yandex.ru>
2022-01-20 21:24:43 +0300

    add some methods

---
+++ src/main/java/lemsolaris/services/flightDispatcher/Resources.java
@@ -0,0 +1,9 @@
+package lemsolaris.services.flightDispatcher;
+
+public class Resources {
+
+
+    public void return_resources(int flight_id){
+
+    }
+}
---
+++ src/main/java/lemsolaris/services/reports/ReportsGenerator.java
@@ -0,0 +1,12 @@
+package lemsolaris.services.reports;
+
+public class ReportsGenerator {
+
+    public void create_report_of_anomaly(int anomaly_id){
+
+    }
+
+    public void create_report_of_tour(int tour_id){
+        
+    }
+}
