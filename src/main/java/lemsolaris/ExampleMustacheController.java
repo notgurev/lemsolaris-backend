9eac43525a5d6b01d3526861a851979ef95886d8
HusseinHalifaev <a.alibekovich@yandex.ru>
2022-01-22 20:25:12 +0300

    Update ExampleMustacheController.java

--- src/main/java/lemsolaris/ExampleMustacheController.java
+++ src/main/java/lemsolaris/ExampleMustacheController.java
@@ -1,28 +1,33 @@
 package lemsolaris;
 
-import lemsolaris.auth.User;
-import lemsolaris.repositories.AuthRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lemsolaris.services.internal.AnomalyService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class ExampleMustacheController {
+    final AnomalyService anomalyService;
 
-    @GetMapping("/example")
-    public ModelAndView foo(Map<String, Object> model) {
-        return new ModelAndView("example", model);
+    public ExampleMustacheController(AnomalyService anomalyService) {
+        this.anomalyService = anomalyService;
     }
 
-    @GetMapping("/registrationPage")
-    public ModelAndView register(Map<String, Object> model){
-        return new ModelAndView("/authPage/registrationPage", model);
+    @GetMapping("/exploredAnomaly")
+    public ModelAndView getExploredAnomaly(Map<String, Object> model) {
+        model.put("anomaly", anomalyService.getAnomalyByStatus("Exp"));
+        return new ModelAndView("anomaly", model);
+    }
+
+    @GetMapping("/anomalyReport")
+    public ModelAndView anomalyReport(Map<String, Object> model, @RequestParam(name = "id") int id) {
+        model.put("anomalyReport", anomalyService.getReportAnomaly(id));
+        return new ModelAndView("anomaly", model);
     }
 
 }
