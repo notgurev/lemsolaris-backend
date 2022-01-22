41a855f657830f7659fd8a1e474e6572580b3603
HusseinHalifaev <a.alibekovich@yandex.ru>
2022-01-22 20:25:04 +0300

    delete

--- src/main/java/lemsolaris/repositories/AuthRepository.java
+++
@@ -1,8 +0,0 @@
-package lemsolaris.repositories;
-
-import lemsolaris.auth.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthRepository extends JpaRepository<User, Long> {
-    public User findUserByLoginAndPassword(String login, String password);
-}
