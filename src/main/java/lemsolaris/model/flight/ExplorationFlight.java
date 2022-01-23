711fffe9d71d7e68c253fbbfa0707a88bc372c03
notgurev <notgurev@gmail.com>
2022-01-23 16:49:05 +0300

    Planned Exploration Flight constructor

--- src/main/java/lemsolaris/model/flight/ExplorationFlight.java
+++ src/main/java/lemsolaris/model/flight/ExplorationFlight.java
@@ -1,17 +1,26 @@
 package lemsolaris.model.flight;
 
+import lemsolaris.model.other.Ship;
 import lemsolaris.model.reports.AnomalyReport;
+import lemsolaris.util.Utility;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "exploration_flight")
 @Data
+@NoArgsConstructor
 public class ExplorationFlight extends Flight {
     @OneToOne
     @JoinColumn(name = "report_id", referencedColumnName = "id")
     private AnomalyReport report;
+
+    public ExplorationFlight(Ship ship) {
+        super("Exploration", ship, "Planned", 0, Utility.tomorrow(), null);
+    }
 }
