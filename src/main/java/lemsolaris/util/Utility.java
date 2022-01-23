d7efe3d84cc39dd045f34943db83a5558220049b
notgurev <notgurev@gmail.com>
2022-01-23 16:39:32 +0300

    add tomorrow to Utility

--- src/main/java/lemsolaris/util/Utility.java
+++ src/main/java/lemsolaris/util/Utility.java
@@ -1,5 +1,9 @@
 package lemsolaris.util;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Random;
 
 public class Utility {
@@ -9,5 +13,9 @@ public class Utility {
 
     public static int randomIntInRange(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    public static Timestamp tomorrow() {
+        return Timestamp.valueOf(LocalDateTime.now().plusDays(1));
     }
 }
