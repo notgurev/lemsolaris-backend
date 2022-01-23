1feb2a3172577903fb635eb2397d452fe971e78f
notgurev <notgurev@gmail.com>
2022-01-23 22:24:20 +0300

    add AnomalyService::exploreAnomaly

--- src/main/java/lemsolaris/services/AnomalyService.java
+++ src/main/java/lemsolaris/services/AnomalyService.java
@@ -2,13 +2,18 @@ package lemsolaris.services;
 
 import lemsolaris.model.anomaly.Anomaly;
 import lemsolaris.model.anomaly.AnomalyStatus;
+import lemsolaris.model.anomaly.AnomalyType;
 import lemsolaris.model.anomaly.HazardLevel;
 import lemsolaris.repositories.AnomalyRepository;
+import lemsolaris.util.RandomUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.Optional;
+
+import static lemsolaris.util.RandomUtil.randomFromArray;
+import static lemsolaris.util.RandomUtil.randomIntInRange;
 
 @Service
 public class AnomalyService {
@@ -33,5 +38,18 @@ public class AnomalyService {
             throw new RuntimeException("Anomaly not found by id");
         }
         return a.get();
+    }
+
+    private static final HazardLevel[] hazardLevels = {HazardLevel.Safe, HazardLevel.Dangerous};
+    private static final AnomalyType[] anomalyTypes = {AnomalyType.NonStructural, AnomalyType.Structural};
+
+    public Anomaly exploreAnomaly(Anomaly a) {
+        a.setStatus(AnomalyStatus.Explored);
+        a.setHazardLevel(randomFromArray(hazardLevels));
+        a.setType(RandomUtil.randomFromArray(anomalyTypes));
+        if (a.getHazardLevel() == HazardLevel.Dangerous) {
+            a.setFlightRadius(randomIntInRange(50, 100));
+        }
+        return a;
     }
 }
